5e6aa9fbb50ea5f4078c3c161cbf564820e387c4
miccar <Michael.Carlson2@extendhealth.com>
2015-02-10 12:59:45 -0700

    Added unfinished test. It is unfinished because I need to switch computers.

---
+++ src/IoC.Test/IoCObjectTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace IoC.Test
+{
+    [TestFixture]
+    public class IoCObjectTests
+    {
+        [Test]
+        public void CreateIoCObjectTest()
+        {
+            var ioc = new IoCObject();
+        }
+    }
+}
