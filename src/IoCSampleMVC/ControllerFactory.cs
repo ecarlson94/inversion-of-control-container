c3727ca33a74a6dcc9f430b0e10e1396bb442763
Eric Carlson <e.carlson94@gmail.com>
2015-02-10 22:16:31 -0700

    Restructered the repo folder, specifically the MVC sample project was located in the incorrect place.

---
+++ src/IoCSampleMVC/ControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using IoC;
+
+namespace IoCSampleMVC
+{
+    public class ControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            IController controller;
+
+            try
+            {
+                if (controllerType == null)
+                {
+                    throw new ArgumentNullException("controllerType");
+                }
+
+                if (!typeof (IController).IsAssignableFrom(controllerType))
+                {
+                    throw new ArgumentException(string.Format(
+                        "Type requested is not a controller: {0}",
+                        controllerType.Name),
+                        "controllerType");
+                }
+
+                controller = MyIoCContainer.Container.Resolve(controllerType) as IController;
+            }
+            catch
+            {
+                return null;
+            }
+
+            return controller;
+        }
+    }
+
+    public static class MyIoCContainer
+    {
+        public static IoCContainer Container { get; set; }
+    }
+}
